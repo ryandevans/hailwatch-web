c147a1a2589bba1fd4e6aaf0426c101d3a940b4f
ryandevans <31782817+ryandevans@users.noreply.github.com>
2025-05-27 15:49:05 -0500

    Add backend folder, requirements.tx, and render.yaml

---
+++ backend/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+# Allow frontend access
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def home():
+    return {"status": "Hailwatch API is live!"}
